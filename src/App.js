0ddbb95ec0d526f9ccc855ed87b9bc70ed0d5d97
yoonwoojae <dbsdnwp1004@naver.com>
2022-01-19 14:43:17 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import Button from "./Button";
+import styled from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+
+  function byFn() {
+    console.log("bye");
+  }
+
+  function hiFn() {
+    console.log("hi");
+    return byFn;
+  }
+
+  useEffect(hiFn, []);
+  return <h1> Hello </h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div className="App">
-      <h1>하하하</h1>
-      <Button text="버튼" />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick} >{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
