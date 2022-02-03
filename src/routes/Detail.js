1dae1d057af818acee360a574050319847d9e809
yoonwoojae <dbsdnwp1004@naver.com>
2022-02-03 10:43:20 +0900

    comm

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Home.module.css";
+
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+          <div className={styles.container}>
+            {
+              loading ?
+                  (
+                  <div>
+                    <span>loading...</span>
+                  </div>
+                  ) : 
+                  (
+                  <div>
+                    <img src={ movie.background_image} alt={movie.title}/>
+                    <p>{movie.title}</p>
+                  </div>
+                  )
+                }
+          </div>
+          );
 }
 export default Detail;
